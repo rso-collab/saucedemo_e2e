7143a50bd15d84ec5df8cf457f9c213dbdb03894
rsokolov <roma.sokolov@gmail.com>
2025-10-15 18:15:00 +0200

    Initial commit - Ecosio

--- src/main/java/com/ecosio/saucedemo/pages/BasePage.java
+++ src/main/java/com/ecosio/saucedemo/pages/BasePage.java
@@ -3,6 +3,7 @@ package com.ecosio.saucedemo.pages;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class BasePage {
